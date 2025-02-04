7972bf5e1b344e0c7f1c091250baa37620d292dc
Law-son <buabassahlawson@gmail.com>
2025-02-04 20:37:37 +0000

    fix: fix deployment issue

--- components/layout/footer/Footer2.js
+++ components/layout/footer/Footer2.js
@@ -1,6 +1,10 @@
 import Link from "next/link"
 
 export default function Footer2() {
+    const disableClick = (event) => {
+        event.preventDefault();
+      };
+
     return (
         <>           
             
